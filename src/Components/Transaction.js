60bcd15fec3c17797338f60511c151da5f6b3166
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-25 11:12:32 -0500

    adding starting point to github.com

---
+++ src/Components/Transaction.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function Transaction() {
+  return console.log("hi");
+}
+
+export default Transaction;
---
+++ src/Components/TransactionNewForm.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function TransactionNewForm() {
+  return "hi";
+}
+
+export default TransactionNewForm;
