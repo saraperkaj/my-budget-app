46dabc2d376fc55dd4f5121196e669e5f7ea8360
Sara Perkaj <saraperkaj@pursuit.org>
2022-02-24 16:46:04 -0500

    got the red, green, and white to start working

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -19,18 +19,48 @@ function Transactions() {
   //   return {};
   // }
 
-  const transTotal = transactions.reduce(
-    (prev, current) => prev + current.amount,
-    0
-  );
-
   const transList = transactions.map((transaction, index) => {
     return <Transaction key={index} transaction={transaction} index={index} />;
   });
 
+  //its money in and money out
+
+  //this is how much someone has spent
+  const transTotal = transactions
+    .map((transaction) => Number(transaction.amount))
+    .reduce((prev, current) => prev + current, 0);
+
+  // transactions.reduce(
+  //   (prev, current) => prev + current.amount,
+  //   0
+  // );
+
+  // const divStyleGreen = {
+  //   color: "green",
+  // };
+
+  // const divStyleRed = {
+  //   color: "red",
+  // };
+
+  const total = () => {
+    if (transTotal > 1000) {
+      // console.log("green");
+      return <span style={{ color: "green" }}>{transTotal}</span>;
+    }
+    if (transTotal <= 0) {
+      // console.log("red");
+      return <span style={{ color: "red" }}>{transTotal}</span>;
+    }
+    if (transTotal > 0 && transTotal < 1000) {
+      // console.log("normal");
+      return <span style={{ color: "white" }}>{transTotal}</span>;
+    }
+  };
+
   return (
     <>
-      <div>How much you've spent: ${transTotal}</div>
+      <div>How much you've saved: ${total()}</div>
       <ul>{transList}</ul>
     </>
   );
